f4814a64270885fdf43786911856b113c6b3d80c
Lavalojan <lavalojan0924@gmail.com>
2022-01-10 16:59:48 +0530

    create first component

---
+++ src/components/counter.jsx
@@ -0,0 +1,13 @@
+import { Button } from 'bootstrap';
+import React, { Component } from 'react';
+
+class Counter extends React.Component {
+    render() { 
+        return <div>
+            <h1>Hello World</h1>
+            <button>increment</button>
+            </div>;
+    }
+}
+ 
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import React from 'react'
-import ReactDom from 'react-dom'
+import React from "react";
+import ReactDom from "react-dom";
+import "./index.css";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
+import Counter from "./components/counter";
 
-const element = <h1>Hello World</h1>
-console.log(element);
+ReactDom.render(<Counter/>, document.getElementById("root"));
